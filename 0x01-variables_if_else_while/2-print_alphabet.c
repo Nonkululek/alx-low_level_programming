93679766657e7672c30b1d039005fcf9dda15eb9
Nonkululek <nonkululeko.gugulethu@gmail.com>
2023-02-20 10:54:35 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 /**
  * main - Entry point
- *
- *
+ *followed by new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char c;
-for (c = 'a' ; c <= 'z' ; c++)
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 {
-putchar(c);
+putchar(ch);
 }
 putchar('\n');
 return (0)
