d7048abe98213ffc556a9b9d9cfcbde34e2ca31e
Nonkululek <nonkululeko.gugulethu@gmail.com>
2023-02-20 10:35:43 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,6 +2,7 @@
 #include<time.h>
 #include<stdio.h>
 /* more headers goes there */
+
 /**
  * main - Entry point
  *
@@ -10,7 +11,9 @@
  */
 int main(void)
 {
+
 int n;
+
 srand(time(0));
 n = rand(0 - RAND_MAX / 2;
 if ((n % 10 ! = 0 && n % 10 < 6)
